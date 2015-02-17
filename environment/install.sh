5153d0b3f559123395034e223bc5d02641326c71
zaporylie <jakub@piaseccy.pl>
2015-02-17 08:28:49 +0100

    Update install.sh

--- environment/install.sh
+++ environment/install.sh
@@ -2,6 +2,6 @@
 
 wget https://github.com/zaporylie/drupal-boilerplate/archive/master.zip
 unzip master.zip
-cd drupal-boilerplate-master && mv * .[^.]* ../ && cd ../
+cd drupal-boilerplate-master && mv -f * .[^.]* ../ && cd ../
 rm -Rf drupal-boilerplate-master
 rm master.zip
