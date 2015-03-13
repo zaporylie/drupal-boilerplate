da81e1fb4e2d377d24d67f5845e1cd2a211679c8
Jakub Piasecki <jakub@piaseccy.pl>
2015-03-13 08:35:24 +0100

    Update install.sh

--- environment/install.sh
+++ environment/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 curl -Lk https://github.com/zaporylie/drupal-boilerplate/archive/master.tar.gz |  tar -zxf -
-mv -i drupal-boilerplate-master/* drupal-boilerplate-master/.[^.]* ./
+cp -Ri drupal-boilerplate-master/* drupal-boilerplate-master/.[^.]* ./
 rm -Rf drupal-boilerplate-master
